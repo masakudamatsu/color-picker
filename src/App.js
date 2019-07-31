79a2c6567605bc273712bd0ce918066072ff7c6a
Unknown <m.kudamatsu@gmail.com>
2019-07-31 14:50:08 +0900

    Render the Luminance Setter #2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import './App.css';
-
-function App() {
+import LuminanceSetter from './components/LuminanceSetter.js';
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <main>
+          <LuminanceSetter />
+        </main>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/LuminanceSetter.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class LuminanceSetter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      luminance: '',
+      error: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    if (event.target.value.match(/\D+/g)) {
+      this.setState({
+        error: true
+      });
+    } else {
+      this.setState({
+        luminance: event.target.value,
+        error: false
+      });
+    }
+  }
+  render() {
+    let errorMessage;
+    if (this.state.error) {
+      errorMessage = (
+        <p className="error-message">
+          Please enter a number between 0 and 100 (exclusive).
+        </p>
+      );
+    }
+    return (
+      <section className="ciu-search">
+        <p>
+          Luminance:
+          <input type="text"
+                 className="ciu-search__input"
+                 name="feat_search"
+                 autoComplete="off"
+                 autoFocus
+                 value={this.state.luminance}
+                 onChange={this.handleChange}/>
+          % of pure white
+        </p>
+        {errorMessage}
+      </section>
+    );
+  }
+}
+
+export default LuminanceSetter;
